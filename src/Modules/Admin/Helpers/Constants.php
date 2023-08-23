16d2f287205a443c0fb2d53b1eebae273c452a41
Lê Đình Toản <toanld1905@gmail.com>
2023-08-23 10:37:48 +0700

    fix

--- src/Modules/Admin/Helpers/Constants.php
+++ src/Modules/Admin/Helpers/Constants.php
@@ -5,6 +5,7 @@ use ReflectionClass;
 class Constants
 {
     const ADMIN_TYPE_SUPER = 1;
+    const ADMIN_TYPE_GUEST = 2;
     public static function getClassConstants() {
         $reflectionClass = new ReflectionClass(__CLASS__);
         return $reflectionClass->getConstants();
